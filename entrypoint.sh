79be878dc8215ceec93d7c823062fdb84a119e25
JONATHAN-DEV-01 <jonathanads2006@gmail.com>
2026-03-01 15:03:37 -0300

    Correção do docker e entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "Aguardando Banco de Dados iniciar..."
+echo "Aguardando Banco de Dados iniciar e executando migrações..."
 
-# Opcional: Apenas por segurança, embora o depends_on com service_healthy já devesse resolver no compose
-# Mas o entrypoint garante que as migrações rodem apenas quando o DB estiver 100%
+# Tenta rodar a migração em loop até o banco estar pronto
+until flask db upgrade; do
+  echo "Banco ainda indisponível ou migração falhou... tentando novamente em 2 segundos"
+  sleep 2
+done
 
-echo "Executando migrações do banco de dados..."
-flask db upgrade
+echo "Banco de Dados está pronto e migrações aplicadas!"
 
 echo "Iniciando a aplicação Flask..."
 exec "$@"
